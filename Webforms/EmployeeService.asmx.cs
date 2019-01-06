838b3fbde79593d55b65bca3fb01f10e3f8ea388
DESKTOP-SB3UMDG\Shashank-PC <Shashank2361@yahoo.co.in>
2019-01-06 18:24:58 +0000

    Winform 12

---
+++ Webforms/EmployeeService.asmx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Webforms
+{
+    /// <summary>
+    /// Summary description for EmployeeService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class EmployeeService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public List<EmployeeDataAccessLayer.Employee> GetAllEmployees(/*bool _search, int nd, int rows   , int page   , string sidx   , string sord*/  )
+        {
+
+            return EmployeeDataAccessLayer.GetAllEmployees();
+
+        }
+
+    }
+}
